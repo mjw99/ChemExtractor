8d35dbb224d953465d61c1545e3d6687e3fd1de1
Mark Williamson <mw529@cam.ac.uk>
2014-04-22 11:02:54 +0100

    Add PDF support

--- src/main/java/name/mjw/chemextractor/App.java
+++ src/main/java/name/mjw/chemextractor/App.java
@@ -1,6 +1,13 @@
 package name.mjw.chemextractor;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.util.PDFTextStripper;
 
 import uk.ac.cam.ch.wwmm.oscar.Oscar;
 import uk.ac.cam.ch.wwmm.oscar.chemnamedict.entities.ChemicalStructure;
@@ -8,8 +15,15 @@ import uk.ac.cam.ch.wwmm.oscar.chemnamedict.entities.FormatType;
 import uk.ac.cam.ch.wwmm.oscar.chemnamedict.entities.ResolvedNamedEntity;
 
 public class App {
-	public static void main(String[] args) {
-		String s = "Benzene, Phenol, water";
+	PDDocument doc = null;
+
+	public void main(String[] args) throws IOException {
+
+	}
+
+	public void processStringWithOscar(String s) {
+
+		// String s = "Benzene, Phenol, water";
 
 		Oscar oscar = new Oscar();
 		List<ResolvedNamedEntity> entities = oscar
@@ -24,4 +38,44 @@ public class App {
 			System.out.println();
 		}
 	}
+
+	public void processPDFfromStream(FileInputStream input) {
+
+		try {
+			doc = PDDocument.load(input);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		PDFTextStripper stripper = null;
+		try {
+			stripper = new PDFTextStripper();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			String text = stripper.getText(doc);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public FileInputStream processPDFfromStream(String fileName) {
+		File file = new File(fileName);
+		FileInputStream is = null;
+		try {
+			is = new FileInputStream(file);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return is;
+
+	}
+
 }
--- src/test/java/name/mjw/chemextractor/AppTest.java
+++ src/test/java/name/mjw/chemextractor/AppTest.java
@@ -1,38 +1,15 @@
 package name.mjw.chemextractor;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import org.junit.Test;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+public class AppTest {
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	@Test
+	public void processStringWithOscar() {
+		App app = new App();
+
+		app.processStringWithOscar("Benzene, car, television, phenol");
+	}
+
 }
